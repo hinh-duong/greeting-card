70e95dcab6af1c8f939e31ec13e4c419d1798d49
hinhduong <duonghinh0402@gmail.com>
2023-03-09 01:51:26 +0700

    initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Hải Linh';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy Birthday';
+const titleCard = 'Happy Birthday';
+const contentCard = 'Happy birthday to you! I hope you have a very special day and a wonderful year ahead!';
+
+const giftImage = 'card-image.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
